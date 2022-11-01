578433d05568cd2b5f8324b6d9bc775d205365a1
Alina <linalevel5@gmail.com>
2022-11-01 19:12:31 +0300

    Initial Commit

---
+++ ColorizedApp_LESSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedApp_LESSON
+//
+//  Created by MAcbook on 01.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
